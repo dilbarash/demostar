fa731987b4f40e3c2457817d1d2c65e1d555070c
dilbarash <145140633+dilbarash@users.noreply.github.com>
2024-12-13 10:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const stargazingSpots = [
 
 // Function to fetch weather and AQI data
 async function fetchWeatherAndAQI(lat, lon) {
-    const weatherApiKey = 'e943ba1a3f38663ee66ba362f50a008a'; // Replace with your API key
+    const weatherApiKey = 'c2c07ed68408e1730b71769f8740c726'; // Replace with your API key
     const airQualityApiKey = 'a87d60b45493985ee0c842179fd66174a556f4fe'; // Replace with your API key
 
     try {
